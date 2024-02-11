dd87a6ff754b80c3a7d0889ee081a37fb8dbcb2c
Olga Korol <oggi.fine@gmail.com>
2024-02-11 22:31:06 +0300

    file package-lock.json remove from tracking

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "es6": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "airbnb-base",
+        "plugin:unicorn/recommended",
+        "plugin:prettier/recommended",
+        "prettier",
+        "prettier/unicorn"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "no-var": "error",
+        "semi": "error",
+        "indent": "error",
+        "no-multi-spaces": "error",
+        "space-in-parens": "error",
+        "no-multiple-empty-lines": "error",
+        "prefer-const": "error",
+        "no-use-before-define": "error",
+        "unicorn/prevent-abbreviations": "off"
+    }
+};
